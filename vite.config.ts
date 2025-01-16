d46cbaf20ca3ffb59c18707b649afb92c470024a
Ruslan Salmanov <flainof@gmail.com>
2025-01-16 08:10:09 +0300

    feat: setup esbuild settings

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+    esbuild: {
+        jsxFactory: "flactElement",
+        jsxInject: `import { flactElement } from 'flact'`,
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
